e8b4cfb5da5a2e5d06379a79c589da1199e1458d
Давыдов Олег <day-58@mail.ru>
2023-03-01 00:09:14 +0500

    Добавил комменты, убрал пару багов,написал сомнительную процедуру, которую потом по уму бы переделать, изменил тип хранения стилей.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing;
 using System.IO;
-using System.IO.Packaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Forms;
-using System.Windows.Forms.VisualStyles;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using Microsoft.Win32;
 using MenuItem = System.Windows.Controls.MenuItem;
 using MessageBox = System.Windows.MessageBox;
-using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 
 namespace BerestovFirstLab
